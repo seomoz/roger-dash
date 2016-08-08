706f289571fdd9bece21b2af967cb1bf85e04da5
ankan <ankanm@@users.noreply.github.com>
2016-08-08 14:35:49 -0700

    Added server.py and update ports.

---
+++ server.py
@@ -0,0 +1,25 @@
+import main
+import cherrypy
+
+if __name__ == '__main__':
+
+    # Mount the application
+    cherrypy.tree.graft(main, "/")
+
+    # Unsubscribe the default server
+    cherrypy.server.unsubscribe()
+
+    # Instantiate a new server object
+    server = cherrypy._cpserver.Server()
+
+    # Configure the server object
+    server.socket_host = "0.0.0.0"
+    server.socket_port = 8080
+    server.thread_pool = 30
+
+    # Subscribe this server
+    server.subscribe()
+
+    # Start the server engine (Option 1 *and* 2)
+    cherrypy.engine.start()
+    cherrypy.engine.block()
